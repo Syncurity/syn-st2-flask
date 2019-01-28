f4d502b0f6e35e8958f9f6bfc1f4e9de7baae068
Zachary Hart <zach@syncurity.net>
2019-01-28 17:00:54 -0500

    quiet pip and pipenv, leave message if service already setup

--- setup_websvc.sh
+++ setup_websvc.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 echo "===> Ensuring pipenv is installed"
-pip3 install --user pipenv --upgrade
+pip3 install -q --user pipenv --upgrade > /dev/null
 echo "===> Getting your environment set up"
-pipenv install
+pipenv install > /dev/null
 echo "===> Moving Unit files as needed"
 
 if [ ! -f /usr/lib/systemd/system/irflow-integrations-websvc.service ] ; then
     sudo cp irflow-integrations-websvc.service /usr/lib/systemd/system/
     sudo systemctl daemon-reload
+    echo "===> Starting service"
+    ./start_websvc.sh
+else
+    echo "===> Service already set up, nothing to do!"
+    exit 1
 fi
-
-echo "===> Starting service"
-./start_websvc.sh
